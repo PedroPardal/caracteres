6e90faf5366d522e3c7d467e6d2ef6f09ec7a203
PedroPardal <161457769+PedroPardal@users.noreply.github.com>
2024-05-06 20:18:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
     const textarea = document.getElementById('texto');
     const contador = document.getElementById('contador');
+    const resetButton = document.getElementById('resetButton');
 
-    textarea.addEventListener('input', function () {
-        const textoDigitado = textarea.value;
+    let textoDigitado = ''; // Inicializa a variável textoDigitado
+
+    function atualizarContador() {
         const numeroCaracteres = textoDigitado.length;
         contador.textContent = `${numeroCaracteres} caracteres`;
+    }
+
+    textarea.addEventListener('input', function () {
+        textoDigitado = textarea.value;
+        atualizarContador();
+    });
+
+    resetButton.addEventListener('click', function () {
+        // Reseta o conteúdo do textarea para uma string vazia
+        textarea.value = '';
+        textoDigitado = ''; // Reseta a variável textoDigitado
+        atualizarContador(); // Atualiza o contador de caracteres
     });
 });
